12362d471b8f2cbba1975796ca0d396c6f5061d9
Stelen Gabor <gaborstelen@gmail.com>
2019-06-26 15:38:08 +0200

    Creating main.js; testing js

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+
+
+$('#hide').on('click', function() {
+  $('img').hide();
+});
+
+
+//összekötni a jquery-t a html-el
+//[ ] type in a new message that appears in the chat window if I press the "send" button
+//[ ] type in a new message that appears in the chat window if I press the "return" key
